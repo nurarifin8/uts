f52a2c9c2383ff2a1445fcfa7943e2bbf0db9af3
nurarifin8 <117804960+nurarifin8@users.noreply.github.com>
2023-05-11 19:25:58 +0700

    Add files via upload

---
+++ header1.php
@@ -0,0 +1,15 @@
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+<!-- Bootstrap -->
+
+<Link rel="stylesheet" href="./Bootstrap/css/bootstrap.css">
+<Link rel="stylesheet" href="./Bootstrap/css/bootstrap.min.css">
+<Link rel="stylesheet" href="./sweetalert/swetalert.css">
+
+<!-- JQuery and Bootsrap Script Loading -->
+
+<script src="./JQuery?jguery-3.5.1.js"></script>
+<script src="./bootstrap/js/bootsrap.js"></script>
+<script src="./bootstrap/js/bootstrap.min.js"></script>
